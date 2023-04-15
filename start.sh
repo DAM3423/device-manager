4dba0eb76784525623b753ff635aeeea6e4474cb
Dominic Marshall <damdigital3423@gmail.com>
2023-04-15 13:57:10 +0100

    vue js vuetify implementation and shell script

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$1" == "dev" ]; then
+    COMPOSE_FILE=docker-compose.dev.yml
+elif [ "$1" == "prod" ]; then
+    COMPOSE_FILE=docker-compose.prod.yml
+else
+    echo "Invalid argument. Usage: $0 [dev|prod] [rebuild]"
+    exit 1
+fi
+
+if [ "$2" == "rebuild" ]; then
+    docker compose -f ./"$COMPOSE_FILE" up --build
+else
+    docker compose -f ./"$COMPOSE_FILE" up
+fi
